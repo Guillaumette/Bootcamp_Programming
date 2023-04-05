6d0a301de915b2527f2cc9fb737a5585d8196c7e
Guillaumette <anna_kniazeva@bk.ru>
2023-04-05 16:03:22 +0300

    evaluating stuff

---
+++ b4_sotring_by_choice/Program.cs
@@ -0,0 +1,22 @@
+﻿// O(log2(n) + 1)
+int n = Random.Shared.Next(10000000);
+double s = 0;
+int i = 0;
+while (i > 0)
+{
+    s += i;
+    i = i / 2;
+}
+
+// O(n*log2(n)) 
+for (int j = 2; j < n / 2; j++) // n/2 - 2
+{
+    while (i > 0)
+    {
+        s += i;
+        i = i / 2;
+    }
+}
+// (n/2 - 2) * (log2(n) + 1)
+// n/2 * log2(n) + n/2 - 2log2(n) - 2
+// O(n*log2(n))
